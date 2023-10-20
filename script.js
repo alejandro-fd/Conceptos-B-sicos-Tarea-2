6bee6dd68ad192bff2584f25c30a9f8f8e0e5212
alejandroperez <viejoalejo730@gmail.com>
2023-10-19 21:46:58 -0500

    conceptosB-tarea2

---
+++ script.js
@@ -0,0 +1,10 @@
+let num1= 5;
+let num2= 7;
+let resultado1=Number(num1+num2);
+console.log("El resultado de la suma da: ",resultado1);
+let resultado2=Number(num1-num2);
+console.log("El resultado de la resta da: ",resultado2);
+let resultado3=Number(num1*num2);
+console.log("El resultado de la multiplicación da: ",resultado3);    
+let resultado4=Number(num1/num2);
+console.log("El resultado de la división da: ",resultado4);  
